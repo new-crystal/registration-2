b17b8134facd2b0b44ed25f7b9d2775b686907fc
new-crystal <oj3339@naver.com>
2023-08-14 13:24:48 +0900

    qr_layout_all 파일 수정

--- application/views/admin/qr_layout_all.php
+++ application/views/admin/qr_layout_all.php
@@ -1,76 +1,96 @@
-			<!-- Main content -->
-			<div id="nametag_wrapper">
-                <div class="edit_wrapper">
-                    <button id="btnPrint" type="button" class="btn btn-primary" style="margin-left:20px;">Print<?php $num_row ?></button>
-                </div>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap" rel="stylesheet">
 
-				<!-- Content area -->
-				<div class="content" id="nametag">
-                        <div id="printThis">
-                        <?php
-                        $num_int = 1;
-                        foreach($users as $item){
-                            $lang = preg_match("/[\xE0-\xFF][\x80-\xFF][\x80-\xFF]/", $item['nick_name']);
-                            echo '<div class="a4_area">';
-                                echo '<div class="bg_area">';
-                                echo '<div style ="display: none;">'. $num_int .'</div>';
-                                echo '<div class="txt_con">';
-                                        if($item['nt_info'] != ''){
-                                            echo '<div class="org" id="nt_info">' . $item['nt_info'] . '</div>';
-                                        }
-                                        if($lang==0){
-                                            echo '<div class="nick_name lang_en" id="nick_name">' . $item['nick_name'] . '</div>';
-                                        }else{
-                                            echo '<div class="nick_name" id="nick_name">' . $item['nick_name'] . '</div>';
-                                        }
-                                        echo '<div class="org draggable">' . $item['org_nametag'] . '</div>';
-                                        if($item['qr_chk']=='Y'){
-                                            echo '<div id="qrcode" class="draggable"><img src="/assets/images/QR/qrcode_' . $item['registration_no'] . '.jpg"></div>';
-                                        }
-                                        echo '<div class="receipt receipt_name">' . $item['nick_name'] . '</div>';
-                                        echo '<div class="receipt receipt_price">'. number_format($item['fee'])  .'</div>';
-                                    echo '</div>';
-                                echo '</div>';
-                            echo '</div>';
-                            $num_int = $num_int + 1;
-                            }
-                        ?>
-                        </div>
-				</div>
-				<!-- /content area -->
+<style>
+    @font-face {
+        font-family: NanumSquare;
+        src: url("../../../assets/font/NanumSquare-Hv.otf");
+    }
 
-			</div>
-			<!-- /main content -->
+    #nick_name {
+        font-family: NanumSquare;
+    }
+</style>
 
-		</div>
-		<!-- /page content -->
+<!-- Main content -->
+<div id="nametag_wrapper">
+    <div class="edit_wrapper">
+        <button id="btnPrint" type="button" class="btn btn-primary" style="margin-left:20px;">Print<?php $num_row ?></button>
+    </div>
 
-	</div>
-	<!-- /page container -->
-	<style>
-        body{
-            background-color: #fff;
-        }
-    </style>
-	<script>
-        document.getElementById("btnPrint").onclick = function () {
-            printElement(document.getElementById("printThis"));
-        }
-
-        function printElement(elem) {
-            var domClone = elem.cloneNode(true);
-
-            var $printSection = document.getElementById("printSection");
-
-            if (!$printSection) {
-                var $printSection = document.createElement("div");
-                $printSection.id = "printSection";
-                document.body.appendChild($printSection);
+    <!-- Content area -->
+    <div class="content" id="nametag">
+        <div id="printThis">
+            <?php
+            $num_int = 1;
+            foreach ($users as $item) {
+                $lang = preg_match("/[\xE0-\xFF][\x80-\xFF][\x80-\xFF]/", $item['nick_name']);
+                $nicknameLength = mb_strlen($item['nick_name'], "UTF-8");
+                echo '<div class="a4_area">';
+                echo '<div class="bg_area">';
+                echo '<div class="txt_con">';
+                if ($item['nt_info'] != '') {
+                    echo '<div class="org" id="nt_info">' . $item['nt_info'] . '</div>';
+                }
+                if ($lang == 0) {
+                    echo '<div class="nick_name lang_en" id="nick_name">' . $item['nick_name'] . '</div>';
+                } else if ($lang !== 0 && $nicknameLength == 3) {
+                    echo '<div class="nick_name" id="nick_name">' . $item['nick_name'] . '</div>';
+                } else if ($lang !== 0 && $nicknameLength > 3) {
+                    echo '<div class="small_nickname" id="nick_name">' . $item['nick_name'] . '</div>';
+                }
+                echo '<div class="org" id="org">' . $item['org_nametag'] . '</div>';
+                echo '<div id="qrcode" class=""><img src="/assets/images/QR/qrcode_' . $item['registration_no'] . '.jpg"></div>';
+                echo '<div class="receipt receipt_num_1">' . $item['registration_no'] . '</div>';
+                echo '<div class="receipt receipt_name">' . $item['nick_name'] . '</div>';
+                echo '<div class="receipt receipt_price">' . number_format($item['fee']) . '</div>';
+                echo '<div class="receipt receipt_num_2">' . $item['registration_no'] . '</div>';
+                echo '<div class="receipt receipt_small small_nick">' . $item['nick_name'] . '</div>';
+                echo '<div class="receipt receipt_small smaill_ln">' . $item['ln'] . '</div>';
+                echo '<div class="receipt receipt_small small_sn">' . $item['sn'] . '</div>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+                $num_int = $num_int + 1;
             }
+            ?>
+        </div>
+    </div>
+    <!-- /content area -->
 
-            $printSection.innerHTML = "";
-            $printSection.appendChild(domClone);
-            window.print();
+</div>
+<!-- /main content -->
+
+</div>
+<!-- /page content -->
+
+</div>
+<!-- /page container -->
+<style>
+    body {
+        background-color: #fff;
+    }
+</style>
+<script>
+    document.getElementById("btnPrint").onclick = function() {
+        printElement(document.getElementById("printThis"));
+    }
+
+    function printElement(elem) {
+        var domClone = elem.cloneNode(true);
+
+        var $printSection = document.getElementById("printSection");
+
+        if (!$printSection) {
+            var $printSection = document.createElement("div");
+            $printSection.id = "printSection";
+            document.body.appendChild($printSection);
         }
-    </script>
+
+        $printSection.innerHTML = "";
+        $printSection.appendChild(domClone);
+        window.print();
+    }
+</script>
 </body>
