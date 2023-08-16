07cac004103b3a44e90ae18b2b188c43182ce9e4
new-crystal <oj3339@naver.com>
2023-08-16 15:50:25 +0900

    hotfix message

--- application/views/admin/send_msm.php
+++ application/views/admin/send_msm.php
@@ -47,17 +47,22 @@ if ($err) {
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => "POST",
         CURLOPT_POSTFIELDS => array(
-            'phone' =>  $phone, 'callback' => '01090224867', 'message' => '안녕하세요. ' . $nick_name . '  님[접수번호 :' . $registration_no . ']
-        2023년 서울아산병원 당뇨병개원의 연수강
-        좌에 참석 해주셔서 감사합니다.
-        현장 등록데스크에 방문하여 위 QR코드를 제
-        시 후 네임택을 수령 부탁드립니다.
-        하기 QR코드는 출석 태깅 QR코드가 아니며,
-        출석 태킹은 네임택 수령 후 네임택 QR코드
-        을 사용 부탁 드립니다.
-        -운영사무국 드림-
-        ', 'refkey' => 'RESTAPITEST1548722798', 'subject' => ' 2023년 서울아산병원 당뇨병개원
-        의 연수강좌 현장 사전 안내', 'image_cnt' =>
+            'phone' =>  $phone, 'callback' => '01090224867', 'message' => '안녕하세요. 정보기술팀 입니다.' . $nick_name . '  선생님[접수번호 :' . $registration_no . ']
+            본 문자는 이번 정보기술팀 자체 QR네임택 시스템 개발 테스트로 진행되는 문자입니다.
+            테스트 진행에 도움 주시면 감사하겠습니다.
+            * 테스트 진행 일시 : 2023년 8월 17일 / 09:00~10:00
+            * 3층 정보기술팀 방문하시여 전달 받으신 QR코드를 스탠하시고 네임택을 수령 부탁드립니다.
+
+대한내분비학회 2023년 개원의 연수강좌에 참석 해주셔서 감사합니다.
+
+현장 등록데스크에 방문하여 위 QR코드를 제시 후 네임택을 수령 부탁드립니다.
+
+위 QR코드는 출석 태깅 QR코드가 아니며, 출석 태킹은 네임택 수령 후 네임택 QR코드을 사용 부탁 드립니다.
+
+기타 문의 사항은 02-2285-2579로 연락 주시면 감사하겠습니다.
+
+-운영사무국 드림-
+        ', 'refkey' => 'RESTAPITEST1548722798', 'subject' => 'QR 네임택 시스템 테스트 도움요청', 'image_cnt' =>
             '1', '
              images0' => new CURLFILE('assets/images/QR/qrcode_' . $registration_no . '.jpg')
         ),
