a4d0244d334a1cdc12dbaaf1b143636c8153be32
new-crystal <oj3339@naver.com>
2023-08-23 09:16:10 +0900

    page 1차 수정

--- application/views/admin/access.php
+++ application/views/admin/access.php
@@ -1,51 +1,63 @@
+<?php
+$firstName = "";
+$lastName = "";
+if (isset($user['first_name'])) {
+    $firstName = $user['first_name'];
+}
+if (isset($user['last_name'])) {
+    $lastName = $user['last_name'];
+}
+$en_name = $firstName . " " . $lastName
+?>
+
 <script src="https://cdn.tailwindcss.com"></script>
 <script type="text/javascript" src="/assets/js/admin/lecture_history.js"></script>
 <style>
-.qr-info-table {
-    margin-top: 1rem;
-    border: 2px solid #eee;
-    border-collapse: collapse;
-    width: 50%;
-}
+    .qr-info-table {
+        margin-top: 1rem;
+        border: 2px solid #eee;
+        border-collapse: collapse;
+        width: 50%;
+    }
 
-.qr-info-table th {
-    background-color: #1d3557;
-    border-color: #1d3557;
-    color: #fff !important;
-    font-size: 1.7rem;
-    line-height: 2.5rem;
-    font-weight: 600;
-}
+    .qr-info-table th {
+        background-color: #1d3557;
+        border-color: #1d3557;
+        color: #fff !important;
+        font-size: 1.7rem;
+        line-height: 2.5rem;
+        font-weight: 600;
+    }
 
-.qr-info-table>tr,
-.qr-info-table th {
-    border: 2px solid #eee;
-    text-align: center;
-    font-size: 1.25rem;
-    line-height: 2.5rem;
-}
+    .qr-info-table>tr,
+    .qr-info-table th {
+        border: 2px solid #eee;
+        text-align: center;
+        font-size: 1.25rem;
+        line-height: 2.5rem;
+    }
 
-.qr-info-table td {
-    border: 1px solid #eee;
-    text-align: left;
-    font-size: 1.5rem;
-    line-height: 2.5rem;
-    padding-left: 4rem;
-    display: flex;
-    align-items: center;
-    height: 5rem;
-    font-weight: bold;
-}
+    .qr-info-table td {
+        border: 1px solid #eee;
+        text-align: left;
+        font-size: 1.5rem;
+        line-height: 2.5rem;
+        padding-left: 4rem;
+        display: flex;
+        align-items: center;
+        height: 5rem;
+        font-weight: bold;
+    }
 
-.qr-info-table tr {
-    height: 5rem;
-    padding: 4px 8px;
-}
+    .qr-info-table tr {
+        height: 5rem;
+        padding: 4px 8px;
+    }
 
-#open {
-    background-color: #1d3557;
-    float: right;
-}
+    #open {
+        background-color: #1d3557;
+        float: right;
+    }
 </style>
 
 <div class="page-container">
@@ -61,22 +73,17 @@
         <div class="content">
             <div class="panel panel-flat">
                 <div>
-                    <button class="w-[150px] h-[40px] bg-slate-300 mt-20 hover:bg-slate-400 active:bg-slate-500"
-                        type="button" id="open">새창</button>
+                    <button class="w-[150px] h-[40px] bg-slate-300 mt-20 hover:bg-slate-400 active:bg-slate-500" type="button" id="open">새창</button>
                 </div>
-                <form action="/admin/access" id="qr_form" name="qr_form"
-                    class="w-full h-screen flex flex-col items-center justify-center bg-slate-50">
+                <form action="/admin/access" id="qr_form" name="qr_form" class="w-full h-screen flex flex-col items-center justify-center bg-slate-50">
 
                     <div class="w-2/5 flex flex-col items-center justify-center translate-y-60">
-
+                        <?php print_r($firstName) ?>
                         <h1 class="text-5xl mt-32 font-semibold -translate-y-20">QR CODE 입력 </h1>
                         <h6 class="text-3xl mt-20 -translate-y-20">커서를 텍스트박스 안에 놓고 QR 코드 스캐너를 사용하세요.</h6>
-                        <input id="qrcode" name="qrcode" class="w-[400px] h-[50px] mt-20 p-3 -translate-y-20"
-                            type="text" autofocus />
+                        <input id="qrcode_input" name="qrcode" class="w-[400px] h-[50px] mt-20 p-3 -translate-y-20" type="text" autofocus />
 
-                        <button
-                            class="w-[150px] h-[40px] bg-slate-300 mt-20 mb-20 hover:bg-slate-400 active:bg-slate-500 -translate-y-20 text-black"
-                            type="submit" id="submit">등록</button>
+                        <button class="w-[150px] h-[40px] bg-slate-300 mt-20 mb-20 hover:bg-slate-400 active:bg-slate-500 -translate-y-20 text-black" type="submit" id="submit">등록</button>
 
                     </div>
 
@@ -96,14 +103,13 @@
                             <tr>
                                 <th>영문 이름</th>
                                 <td id="en_name" class="qr_text">
-                                    <?php if (isset($user['first_name'])) echo $user['first_name'] ?>
-                                    <?php if (isset($user['last_name'])) echo $user['last_name'] ?>
+                                    <?php if (isset($user['first_name'])) echo $user['first_name'] . " " . $user['last_name']; ?>
                                 </td>
                             </tr>
                             <tr>
                                 <th>이름</th>
                                 <td id="name" class="qr_text">
-                                    <?php if (isset($user['nick_name'])) echo $user['nick_name'] ?></td>
+                                    <?php if (isset($user['name_kor'])) echo $user['name_kor'] ?></td>
                             </tr>
                             <tr>
                                 <th>참가구분</th>
@@ -157,130 +163,132 @@
 <!-- /page container -->
 
 <script>
-const form = document.querySelector("#qr_form");
-const qrcode = document.querySelector("#qrcode");
-const submit = document.querySelector("#submit");
-const qrTexts = document.querySelectorAll(".qr_text")
-const table = document.querySelector(".qr-info-table")
-const open = document.querySelector("#open")
-const name = document.querySelector("#name")
-const EnName = document.querySelector("#en_name")
-const org = document.querySelector("#org")
-const category = document.querySelector("#category")
-const memo = document.querySelector("#memo")
-const number = document.querySelector("#number")
-const etc1 = document.querySelector("#etc1")
-const etc2 = document.querySelector("#etc2")
-const etc3 = document.querySelector("#etc3")
-var childWindow;
-let qrvalue = "";
+    const form = document.querySelector("#qr_form");
+    const qrcode = document.querySelector("#qrcode_input");
+    const submit = document.querySelector("#submit");
+    const qrTexts = document.querySelectorAll(".qr_text")
+    const table = document.querySelector(".qr-info-table")
+    const open = document.querySelector("#open")
+    const name = document.querySelector("#name")
+    const enName = document.querySelector("#en_name")
+    const org = document.querySelector("#org")
+    const category = document.querySelector("#category")
+    const memo = document.querySelector("#memo")
+    const number = document.querySelector("#number")
+    const etc1 = document.querySelector("#etc1")
+    const etc2 = document.querySelector("#etc2")
+    const etc3 = document.querySelector("#etc3")
+    var childWindow;
+    let qrvalue = "";
 
-qrcode.focus();
-
-
-function openQR() {
-    const url = `/qrcode/open`
-    if (childWindow && !childWindow.closed) {
-        childWindow = null;
-    } else {
-        childWindow = window.open(url, 'ChildWindow', 'width=400,height=300');
-    }
-}
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    qrvalue = qrcode.value
-    fetchData(qrcode.value)
-    qrcode.value = "";
     qrcode.focus();
-})
-
-function fetchData(qrcode) {
-    // Ajax 요청 수행
-    fetch(`/admin/access?qrcode=${qrcode}`)
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const htmlDocument = parser.parseFromString(data, 'text/html');
-            if (htmlDocument.querySelector("#number").innerText.replace(/<br\s*\/?>/gi, "").replace(/\s/g,
-                    "")) {
-                number.innerText = htmlDocument.querySelector("#number").innerText.replace(/<br\s*\/?>/gi, "")
-                    .replace(
-                        /\s/g, "");
-                EnName.innerText = htmlDocument.querySelector("#en_name").innerText.replace(/<br\s*\/?>/gi, "")
-                name.innerText = htmlDocument.querySelector("#name").innerText.replace(/<br\s*\/?>/gi, "").replace(
-                    /\s/g, "");
-                org.innerText = htmlDocument.querySelector("#org").innerText.replace(/<br\s*\/?>/gi, "").replace(
-                    /\s/g,
-                    "");
-                category.innerText = htmlDocument.querySelector("#category").innerText.replace(/<br\s*\/?>/gi, "")
-                    .replace(/\s/g, "");
-                memo.innerText = htmlDocument.querySelector("#memo").innerText.replace(/<br\s*\/?>/gi, "").replace(
-                    /\s/g, "");
-                etc1.innerText = htmlDocument.querySelector("#etc1").innerText.replace(/<br\s*\/?>/gi, "").replace(
-                    /\s/g, "");
-                etc2.innerText = htmlDocument.querySelector("#etc2").innerText.replace(/<br\s*\/?>/gi, "").replace(
-                    /\s/g, "");
-                etc3.innerText = htmlDocument.querySelector("#etc3").innerText.replace(/<br\s*\/?>/gi, "").replace(
-                    /\s/g, "");
-            } else {
-                number.innerText = qrvalue
-                name.innerText = "없는 QR입니다."
-                org.innerText = ""
-                category.innerText = ""
-                etc1.innerText = ""
-                throw new Error("없는 QR입니다.");
-            }
-        }).then((data) => {
-            executeFunctionInChildWindow(qrcode);
-        }).then(() => {
-            // window.open(`https://reg2.webeon.net/qrcode/print_file?registration_no=${qrvalue}`, "_blank")
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-}
 
 
-function executeFunctionInChildWindow(data) {
-
-    if (childWindow && !childWindow.closed) {
-        childWindow.postMessage({
-            qrcode: data
-        }, '*');
-    } else {
-
+    function openQR() {
+        const url = `/qrcode/open`
+        if (childWindow && !childWindow.closed) {
+            childWindow = null;
+        } else {
+            childWindow = window.open(url, 'ChildWindow', 'width=400,height=300');
+        }
     }
-}
 
-// 자식 창으로부터의 메시지를 받아 처리하는 함수
-function receiveMessage(event) {
-    if (event.data === "child") {
-
-    }
-}
-
-
-function hideText() {
-    qrTexts.forEach((text) => {
-        text.textContent = "";
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        qrvalue = qrcode.value
+        fetchData(qrcode.value)
+        qrcode.value = "";
+        qrcode.focus();
     })
-}
 
-open.addEventListener("click", () => {
-    openQR()
-})
-
-// 메시지 이벤트 리스너 등록
-window.addEventListener('message', (e) => {
-    // childWindow = null;
-    receiveMessage(e)
-}, false);
-
-window.onload = () => {
-    if (qrvalue) {
-        number.innerText = qrvalue
+    function fetchData(qrcode) {
+        // Ajax 요청 수행
+        fetch(`/admin/access?qrcode=${qrcode}`)
+            .then(response => response.text())
+            .then(data => {
+                const parser = new DOMParser();
+                const htmlDocument = parser.parseFromString(data, 'text/html');
+                if (htmlDocument.querySelector("#number").innerText.replace(/<br\s*\/?>/gi, "").replace(/\s/g,
+                        "")) {
+                    number.innerText = htmlDocument.querySelector("#number").innerText.replace(/<br\s*\/?>/gi, "")
+                        .replace(
+                            /\s/g, "");
+                    enName.innerText = htmlDocument.querySelector("#en_name").innerText.replace(/<br\s*\/?>/gi, "")
+                        .replace(
+                            /\s/g, "");
+                    name.innerText = htmlDocument.querySelector("#name").innerText.replace(/<br\s*\/?>/gi, "").replace(
+                        /\s/g, "");
+                    org.innerText = htmlDocument.querySelector("#org").innerText.replace(/<br\s*\/?>/gi, "").replace(
+                        /\s/g,
+                        "");
+                    category.innerText = htmlDocument.querySelector("#category").innerText.replace(/<br\s*\/?>/gi, "")
+                        .replace(/\s/g, "");
+                    memo.innerText = htmlDocument.querySelector("#memo").innerText.replace(/<br\s*\/?>/gi, "").replace(
+                        /\s/g, "");
+                    etc1.innerText = htmlDocument.querySelector("#etc1").innerText.replace(/<br\s*\/?>/gi, "").replace(
+                        /\s/g, "");
+                    etc2.innerText = htmlDocument.querySelector("#etc2").innerText.replace(/<br\s*\/?>/gi, "").replace(
+                        /\s/g, "");
+                    etc3.innerText = htmlDocument.querySelector("#etc3").innerText.replace(/<br\s*\/?>/gi, "").replace(
+                        /\s/g, "");
+                } else {
+                    number.innerText = qrvalue
+                    name.innerText = "없는 QR입니다."
+                    org.innerText = ""
+                    category.innerText = ""
+                    etc1.innerText = ""
+                    throw new Error("없는 QR입니다.");
+                }
+            }).then((data) => {
+                executeFunctionInChildWindow(qrcode);
+            }).then(() => {
+                // window.open(`https://reg2.webeon.net/qrcode/print_file?registration_no=${qrvalue}`, "_blank")
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
     }
-}
+
+
+    function executeFunctionInChildWindow(data) {
+
+        if (childWindow && !childWindow.closed) {
+            childWindow.postMessage({
+                qrcode: data
+            }, '*');
+        } else {
+
+        }
+    }
+
+    // 자식 창으로부터의 메시지를 받아 처리하는 함수
+    function receiveMessage(event) {
+        if (event.data === "child") {
+
+        }
+    }
+
+
+    function hideText() {
+        qrTexts.forEach((text) => {
+            text.textContent = "";
+        })
+    }
+
+    open.addEventListener("click", () => {
+        openQR()
+    })
+
+    // 메시지 이벤트 리스너 등록
+    window.addEventListener('message', (e) => {
+        // childWindow = null;
+        receiveMessage(e)
+    }, false);
+
+    window.onload = () => {
+        if (qrvalue) {
+            number.innerText = qrvalue
+        }
+    }
 </script>
 </body>
--- application/views/qr_open.php
+++ application/views/qr_open.php
@@ -1,24 +1,24 @@
 <script src="https://cdn.tailwindcss.com"></script>
 <style>
-.text_box {
-    font-size: 7rem;
-    color: #000;
-    z-index: 49;
-    font-weight: 600;
-    animation: fadeIn 2s;
-}
-
-@keyframes fadeIn {
-    0% {
-        opacity: 0;
-        transform: translate3d(0, 0, -100%);
+    .text_box {
+        font-size: 7rem;
+        color: #000;
+        z-index: 49;
+        font-weight: 600;
+        animation: fadeIn 2s;
     }
 
-    to {
-        opacity: 1;
-        transform: translateZ(0);
+    @keyframes fadeIn {
+        0% {
+            opacity: 0;
+            transform: translate3d(0, 0, -100%);
+        }
+
+        to {
+            opacity: 1;
+            transform: translateZ(0);
+        }
     }
-}
 </style>
 <div class="w-full h-screen flex flex-col items-center justify-center">
     <div class="page_1">
@@ -27,8 +27,8 @@
     <div class="page_2" style="display: none;">
         <img class="absolute top-0 left-0" style="z-index: -99;" src="../../assets/images/name_org.png" />
         <div class="text_box">
-            <div id="nickname" class="relative z-50" style="top: 44px;">
-                <?php if (isset($users['nick_name'])) echo $users['nick_name'] ?>
+            <div id="name_kor" class="relative z-50" style="top: 44px;">
+                <?php if (isset($users['name_kor'])) echo $users['name_kor'] ?>
             </div>
         </div>
         <div class="text_box">
@@ -39,37 +39,37 @@
 </div>
 
 <script>
-const page1 = document.querySelector(".page_1");
-const page2 = document.querySelector(".page_2")
-const nickname = document.querySelector("#nickname")
-const org = document.querySelector("#org")
+    const page1 = document.querySelector(".page_1");
+    const page2 = document.querySelector(".page_2")
+    const nickname = document.querySelector("#nickname")
+    const org = document.querySelector("#org")
 
-function childFunction(data) {
-    if (data.qrcode) {
-        window.location.href = `/qrcode/open?qrcode=${data.qrcode}`
-        window.opener.postMessage("child", '*');
+    function childFunction(data) {
+        if (data.qrcode) {
+            window.location.href = `/qrcode/open?qrcode=${data.qrcode}`
+            window.opener.postMessage("child", '*');
+        }
     }
-}
 
-window.onload = () => {
-    page1.style.display = "";
-    page2.style.display = "none";
-    if (window.location.search) {
-        page1.style.display = "none";
-        page2.style.display = "";
-        setTimeout(() => {
-            page1.style.display = "";
-            page2.style.display = "none";
-        }, 10000)
+    window.onload = () => {
+        page1.style.display = "";
+        page2.style.display = "none";
+        if (window.location.search) {
+            page1.style.display = "none";
+            page2.style.display = "";
+            setTimeout(() => {
+                page1.style.display = "";
+                page2.style.display = "none";
+            }, 10000)
+        }
     }
-}
 
 
 
-window.addEventListener('message', function(event) {
+    window.addEventListener('message', function(event) {
 
-    if (event.data) {
-        childFunction(event.data);
-    }
-});
+        if (event.data) {
+            childFunction(event.data);
+        }
+    });
 </script>
