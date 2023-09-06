8d3a44eef76d4feadc71ba0ef8813552aa2ee4a2
new-crystal <oj3339@naver.com>
2023-09-06 16:58:34 +0900

    staff 명찰

--- application/views/qr_staff_print.php
+++ application/views/qr_staff_print.php
@@ -147,7 +147,6 @@ body {
 </style>
 <script>
 document.getElementById("btnPrint").onclick = function() {
-    const id = "<?php echo $users['registration_no']; ?>";
 
     // window.location.href = `https://reg2.webeon.net/qrcode/print_file?registration_no=${id}`
     printElement(document.getElementById("printThis"));
@@ -173,64 +172,5 @@ function printElement(elem) {
 }
 </script>
 
-<script>
-//Make the DIV element draggagle:
-dragElement(document.getElementById("qrcode"));
-dragElement(document.getElementById("org"));
-dragElement(document.getElementById("nick_name"));
 
-function dragElement(elmnt) {
-    var pos1 = 0,
-        pos2 = 0,
-        pos3 = 0,
-        pos4 = 0;
-    if (document.getElementById(elmnt.id)) {
-        /* if present, the header is where you move the DIV from:*/
-        document.getElementById(elmnt.id).onmousedown = dragMouseDown;
-    } else {
-        /* otherwise, move the DIV from anywhere inside the DIV:*/
-        elmnt.onmousedown = dragMouseDown;
-    }
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
-</script>
-<script src="/ckeditor/ckeditor.js"></script>
-<script>
-// Replace the <textarea id="editor1"> with a CKEditor 4
-// instance, using default configuration.
-//        CKEDITOR.replace( 'editor1' );
-
-// Turn off automatic editor creation first.
-CKEDITOR.disableAutoInline = true;
-CKEDITOR.inline('editor1');
-</script>
 </body>
